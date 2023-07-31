492e184cadcbd4225895f2fe961038676eaebd58
Rafal Halama <rafau.halama@gmail.com>
2023-07-31 09:25:50 +0200

    components generated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LayoutModule } from './layout/layout.module';
+import { ViewContactComponent } from './view-contact/view-contact.component';
+import { ContactListComponent } from './contact-list/contact-list.component';
+import { CreateContactComponent } from './create-contact/create-contact.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, ViewContactComponent, ContactListComponent, CreateContactComponent],
   imports: [BrowserModule, AppRoutingModule, LayoutModule],
   bootstrap: [AppComponent],
 })
