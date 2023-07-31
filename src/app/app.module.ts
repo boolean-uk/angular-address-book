28dd39801f847ce4b24d2063cb3929a1b09d821a
MiloszSobczyk <m.sobczyk.official@gmail.com>
2023-07-31 11:14:13 +0200

    Add basic components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LayoutModule } from './layout/layout.module';
-import { ContactsListComponent } from './contacts-list/contacts-list.component';
+import { ContactFormComponent } from './contact-form/contact-form.component';
+import { ContactListComponent } from './contact-list/contact-list.component';
 import { ContactViewComponent } from './contact-view/contact-view.component';
-import { ContactCreationComponent } from './contact-creation/contact-creation.component';
 
 @NgModule({
-  declarations: [AppComponent, ContactsListComponent, ContactViewComponent, ContactCreationComponent],
+  declarations: [AppComponent, ContactFormComponent, ContactListComponent, ContactViewComponent],
   imports: [BrowserModule, AppRoutingModule, LayoutModule],
   bootstrap: [AppComponent],
 })
