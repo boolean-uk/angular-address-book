5408fcfb90c7409d1481f4d1cfabef225488a8ef
Rafal Halama <rafau.halama@gmail.com>
2023-07-31 09:55:12 +0200

    view contact implemented

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LayoutModule } from './layout/layout.module';
-import { ViewContactComponent } from './view-contact/view-contact.component';
 import { ContactListComponent } from './contact-list/contact-list.component';
 import { CreateContactComponent } from './create-contact/create-contact.component';
+import { ViewContactComponent } from './view-contact/view-contact.component';
+import { ContactService } from './contact.service';
 
 @NgModule({
-  declarations: [AppComponent, ViewContactComponent, ContactListComponent, CreateContactComponent],
-  imports: [BrowserModule, AppRoutingModule, LayoutModule],
+  declarations: [
+    AppComponent,
+    ContactListComponent,
+    CreateContactComponent,
+    ViewContactComponent,
+  ],
+  imports: [BrowserModule, AppRoutingModule, LayoutModule, ReactiveFormsModule],
   bootstrap: [AppComponent],
+  providers: [ContactService],
 })
 export class AppModule {}
--- src/app/view-contact/view-contact.component.ts
+++ src/app/view-contact/view-contact.component.ts
@@ -1,8 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { Contact, ContactService } from '../contact.service';
+import { ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-view-contact',
   templateUrl: './view-contact.component.html',
   styleUrls: ['./view-contact.component.css'],
 })
-export class ViewContactComponent {}
+export class ViewContactComponent implements OnInit {
+  contact: Contact | undefined;
+  constructor(
+    private _contactService: ContactService,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit() {
+    this.route.params.subscribe((params: Params) => {
+      let x = this._contactService.getContactById(params['id']);
+      console.log(x);
+      if (x !== undefined) {
+        this.contact = x;
+      }
+    });
+  }
+}
