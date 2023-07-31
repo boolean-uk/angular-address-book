0b7e9e10add6ff36ba01c59e232ff55644290c36
Julia Girejko <julia.girejko@talan.com>
2023-07-31 16:10:58 +0200

    view contact added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -15,7 +15,7 @@ const routes: Routes = [
   { path: 'new-contact',
     component: NewContactComponent
   },
-  { path: 'view-contact',
+  { path: 'view-contact/:id',
     component: ViewContactComponent
   }
 ]
--- src/app/layout/view-contact/view-contact.component.ts
+++ src/app/layout/view-contact/view-contact.component.ts
@@ -1,21 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Contact, ContactService } from '../contact.service';
 import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-view-contact',
   templateUrl: './view-contact.component.html',
   styleUrls: ['./view-contact.component.css']
 })
-export class ViewContactComponent {
+export class ViewContactComponent implements OnInit {
   contacts: Contact[] = []
   id: number | null = Number(this.route.snapshot.paramMap.get('id'));
+  paramsSub: Subscription | null = null;
+  contact: Contact | null | undefined = null;
 
   constructor(private readonly contactService: ContactService, private readonly route: ActivatedRoute,) {
     this.contacts = this.contactService.contactList;
     // this.id = this.contactService.getContactById(this.id)
   }
 
+  ngOnInit() {
+    console.log('hello from ngOnInit');
+    this.paramsSub = this.route.params.subscribe((params) => {
+      this.id = params['id'];
+      this.contact = this.contactService.getContactById(params['id'])
+      console.log('contact: ', this.contact);
+    });
+  }
 
+  ngOnDestroy(): void {
+    console.log('on destroy');
+    if (this.paramsSub) {
+      this.paramsSub.unsubscribe();
+    }
+  }
 
 }
