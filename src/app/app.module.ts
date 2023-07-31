ee67356a112cf9e5a59ce2d7abc5f4acbd2528f2
Alicja Bobko <alicja.bobko@talan.com>
2023-07-31 11:25:41 +0200

    feat: routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LayoutModule } from './layout/layout.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { WelcomePageComponent } from './welcome-page/welcome-page.component';
+import { ContactsListComponent } from './contacts-list/contacts-list.component';
+import { NewContactComponent } from './new-contact/new-contact.component';
+import { ViewContactComponent } from './view-contact/view-contact.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, WelcomePageComponent, ContactsListComponent, NewContactComponent, ViewContactComponent, NotFoundComponent],
   imports: [BrowserModule, AppRoutingModule, LayoutModule, BrowserAnimationsModule],
   bootstrap: [AppComponent],
 })
