51a916aab6ddc1268367262827407fe0988da696
Rafal Halama <rafau.halama@gmail.com>
2023-07-31 09:39:47 +0200

    contact service implemented

--- src/app/contact.service.ts
+++ src/app/contact.service.ts
@@ -1,9 +1,31 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs/internal/BehaviorSubject';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-export class ContactService {
+export class ContactServiceService {
+  private _contacts: Contact[] = [];
+  private _contactSubject = new BehaviorSubject<Contact[]>([]);
+  constructor() {}
 
-  constructor() { }
+  addContact(contact: Contact) {
+    this._contacts.push(contact);
+    this._contactSubject.next(this._contacts);
+  }
+  getContacts() {
+    return this._contactSubject.asObservable();
+  }
+
+  getContactById(id: string) {
+    return this._contacts.find((contact) => contact.id === id);
+  }
+}
+
+export interface Contact {
+  id: string;
+  firstName: string;
+  lastName: string;
+  street: string;
+  city: string;
 }
