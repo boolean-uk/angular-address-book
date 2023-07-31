af534c7775d46087edd672bb480efe572b09ffde
Kinga <kinga.hernet@talan.com>
2023-07-31 18:20:41 +0200

    Added app module and layout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactsListComponent } from './contacts-list/contacts-list.component';
+import { NewContactComponent } from './new-contact/new-contact.component';
+import { ViewContactComponent } from './view-contact/view-contact.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/contacts', pathMatch: 'full' },
+  { path: 'contacts', component: ContactsListComponent },
+  { path: 'new-contact', component: NewContactComponent },
+  { path: 'view-contact', component: ViewContactComponent },
+  { path: 'view-contact/:id', component: ViewContactComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
