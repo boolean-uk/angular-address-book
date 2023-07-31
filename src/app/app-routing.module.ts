5da863a04b66b2c948d927b73f9dd904a531d3cf
Julia Girejko <julia.girejko@talan.com>
2023-07-31 11:38:09 +0200

    components added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MenuComponent } from './layout/menu/menu.component';
+import { ContactListComponent } from './layout/contact-list/contact-list.component';
+import { NewContactComponent } from './layout/new-contact/new-contact.component';
+import { ViewContactComponent } from './layout/view-contact/view-contact.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home',
+    component: MenuComponent
+  },
+  { path: 'contact-list',
+    component: ContactListComponent
+  },
+  { path: 'new-contact',
+    component: NewContactComponent
+  },
+  { path: 'view-contact',
+    component: ViewContactComponent
+  }
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { MenuComponent } from './menu/menu.component';
+import { ContactListComponent } from './contact-list/contact-list.component';
+import { NewContactComponent } from './new-contact/new-contact.component';
+import { ViewContactComponent } from './view-contact/view-contact.component';
 
 @NgModule({
-  declarations: [MenuComponent],
+  declarations: [MenuComponent, ContactListComponent, NewContactComponent, ViewContactComponent],
   imports: [CommonModule, RouterModule],
-  exports: [MenuComponent],
+  exports: [MenuComponent, ContactListComponent, NewContactComponent, ViewContactComponent],
 })
 export class LayoutModule {}
