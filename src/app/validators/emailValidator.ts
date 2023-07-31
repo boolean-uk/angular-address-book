c0593f51f06acf4341f059425bb1fd611f07a21a
Rafal Halama <rafau.halama@gmail.com>
2023-07-31 12:59:03 +0200

    made email validator clearer

--- src/app/validators/emailValidator.ts
+++ src/app/validators/emailValidator.ts
@@ -4,10 +4,10 @@ import { Observable, of } from 'rxjs';
 export function asyncEmailValidator(
   control: AbstractControl
 ): Observable<ValidationErrors | null> {
-  const emailRegex: RegExp = /^[a-z0-9._%+-]+@[a-z]{7}.[a-z]{2}.[a-z]{2}$/;
+  const emailRegex: RegExp = /^[a-z0-9._%+-]+@boolean.co.uk/;
   const email: string = '' + control.value;
 
-  if (!email.endsWith('@boolean.co.uk') || !emailRegex.test(email)) {
+  if (!emailRegex.test(email)) {
     return of({ asyncEmailValidator: true });
   }
   return of(null);
