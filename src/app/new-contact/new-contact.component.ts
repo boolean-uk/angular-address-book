8564ce49fc8c2a976d974ea0462b106e87447d71
Damian Gurdak <damian.gurdak@talan.com>
2023-07-31 15:20:51 +0300

    add extension

--- src/app/new-contact/new-contact.component.ts
+++ src/app/new-contact/new-contact.component.ts
@@ -13,6 +13,7 @@ export class NewContactComponent implements OnInit {
     lastName: new FormControl(''),
     street: new FormControl(''),
     city: new FormControl(''),
+    email: new FormControl(''),
   })
 
   constructor(private contactsService: ContactsService){
@@ -21,9 +22,33 @@ export class NewContactComponent implements OnInit {
 
   ngOnInit(): void {
     this.contactForm.controls.firstName.addValidators(Validators.required)
+    this.contactForm.controls.firstName.addValidators(Validators.minLength(3))
     this.contactForm.controls.lastName.addValidators(Validators.required)
+    this.contactForm.controls.lastName.addValidators(Validators.minLength(3))
+
+    this.contactForm.controls.firstName.addValidators(this.noSpacesValidator)
+    this.contactForm.controls.lastName.addValidators(this.noSpacesValidator)
+
     this.contactForm.controls.street.addValidators(Validators.required)
     this.contactForm.controls.city.addValidators(Validators.required)
+
+    this.contactForm.controls.email.addValidators(Validators.required)
+    this.contactForm.controls.email.addValidators(this.booleanEmailValidator)
+    this.contactForm.controls.email.addValidators(Validators.email)
+  }
+
+  noSpacesValidator(control:any) {
+    if (control.value && /\s/.test(control.value)) {
+      return { noSpaces: true };
+    }
+    return null;
+  }
+
+  booleanEmailValidator(control:any) {
+    if (control.value && !control.value.endsWith('@boolean.co.uk')) {
+      return { booleanEmail: true };
+    }
+    return null;
   }
 
   submit(event: Event){
