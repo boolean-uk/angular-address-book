6e34fb90408e6b563310475ae3b64c4dc02ee4d2
Rafal Halama <rafau.halama@gmail.com>
2023-07-31 10:25:30 +0200

    on destroy added

--- src/app/contact-list/contact-list.component.ts
+++ src/app/contact-list/contact-list.component.ts
@@ -1,19 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Contact, ContactService } from '../contact.service';
+import { Subscription } from 'rxjs/internal/Subscription';
 
 @Component({
   selector: 'app-contact-list',
   templateUrl: './contact-list.component.html',
   styleUrls: ['./contact-list.component.css'],
 })
-export class ContactListComponent implements OnInit {
+export class ContactListComponent implements OnInit, OnDestroy {
   contacts: Contact[] = [];
 
   constructor(private _contactService: ContactService) {}
-
+  paramsSub: Subscription | null = null;
   ngOnInit() {
-    this._contactService.getContacts().subscribe((contacts) => {
-      this.contacts = contacts;
-    });
+    this.paramsSub = this._contactService
+      .getContacts()
+      .subscribe((contacts) => {
+        this.contacts = contacts;
+      });
+  }
+  ngOnDestroy(): void {
+    if (this.paramsSub) {
+      this.paramsSub.unsubscribe();
+    }
   }
 }
--- src/app/view-contact/view-contact.component.ts
+++ src/app/view-contact/view-contact.component.ts
@@ -1,22 +1,29 @@
-import { Component, Inject, OnInit } from '@angular/core';
+import { Component, Inject, OnDestroy, OnInit } from '@angular/core';
 import { Contact, ContactService } from '../contact.service';
 import { ActivatedRoute, Params } from '@angular/router';
+import { Subscription } from 'rxjs/internal/Subscription';
 
 @Component({
   selector: 'app-view-contact',
   templateUrl: './view-contact.component.html',
   styleUrls: ['./view-contact.component.css'],
 })
-export class ViewContactComponent implements OnInit {
+export class ViewContactComponent implements OnInit, OnDestroy {
   contact: Contact | undefined;
+  paramsSub: Subscription | null = null;
   constructor(
     private _contactService: ContactService,
     private route: ActivatedRoute
   ) {}
 
   ngOnInit() {
-    this.route.params.subscribe((params: Params) => {
+    this.paramsSub = this.route.params.subscribe((params: Params) => {
       this.contact = this._contactService.getContactById(params['id']);
     });
+  }
+  ngOnDestroy(): void {
+    if (this.paramsSub) {
+      this.paramsSub.unsubscribe();
+    }
   }
 }
