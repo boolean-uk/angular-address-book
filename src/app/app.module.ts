f16f98510cbeadeee3657054efc071446f8a2731
MiloszSobczyk <m.sobczyk.official@gmail.com>
2023-07-31 10:52:19 +0200

    added basic modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LayoutModule } from './layout/layout.module';
+import { ContactsListComponent } from './contacts-list/contacts-list.component';
+import { ContactViewComponent } from './contact-view/contact-view.component';
+import { ContactCreationComponent } from './contact-creation/contact-creation.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, ContactsListComponent, ContactViewComponent, ContactCreationComponent],
   imports: [BrowserModule, AppRoutingModule, LayoutModule],
   bootstrap: [AppComponent],
 })
