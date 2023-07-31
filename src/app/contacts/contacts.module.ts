a0a8eb4ebe1873d19b23a9702314c672764dd576
Michal Antoniuk <michal.antoniuk@talan.com>
2023-07-31 15:55:57 +0200

    Add styling using Angular Material and CSS

--- src/app/contacts/contacts.module.ts
+++ src/app/contacts/contacts.module.ts
@@ -1,6 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterModule } from '@angular/router';
 import { ContactListItemComponent } from './contact-list-item/contact-list-item.component';
 import { ContactListComponent } from './contact-list/contact-list.component';
@@ -14,7 +18,15 @@ import { ContactViewComponent } from './contact-view/contact-view.component';
     ContactListItemComponent,
     ContactNewFormComponent,
   ],
-  imports: [CommonModule, ReactiveFormsModule, RouterModule],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    RouterModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    BrowserAnimationsModule,
+  ],
   exports: [
     ContactViewComponent,
     ContactListComponent,
--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -1,11 +1,20 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatListModule } from '@angular/material/list';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { RouterModule } from '@angular/router';
 import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [MenuComponent],
-  imports: [CommonModule, RouterModule],
+  imports: [
+    CommonModule,
+    RouterModule,
+    MatToolbarModule,
+    MatListModule,
+    MatButtonModule,
+  ],
   exports: [MenuComponent],
 })
 export class LayoutModule {}
