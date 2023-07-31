e1db386dc15b0c8f8ecf56ce6092f546c188efc4
karim <01171252@pw.edu.pl>
2023-07-31 13:44:31 +0200

    make it pretty

--- src/app/Contact/contact.service.ts
+++ src/app/Contact/contact.service.ts
@@ -15,15 +15,12 @@ export class ContactService {
 
   addContact(contact: Contact): void {
     contact.id = this.getNextId();
+    this.formatContact(contact);
     this.contacts.push(contact);
   }
 
   getAllContacts(): Contact[] {
     return this.contacts;
-  }
-
-  deleteContact(contact: Contact): void {
-    this.contacts = this.contacts.filter((c) => c.id === contact.id);
   }
 
   findById(id:number): Contact | undefined{
@@ -34,6 +31,20 @@ export class ContactService {
     return this.contacts.length > 0
       ? Math.max(...this.contacts.map((contact) => contact.id)) + 1
       : 1;
+  }
+
+  private formatContact(contact: Contact): void {
+    contact.firstName = this.formatString(contact.firstName);
+    contact.lastName = this.formatString(contact.lastName);
+    contact.street = this.formatString(contact.street);
+    contact.city = this.formatString(contact.city);
+  }
+
+  private formatString(str: string): string {
+    str = str.trim();
+    str = str.replace(/\s+/g, ' ');
+    str = str.replace(/(^| )\S/g, (match) => match.toUpperCase());
+    return str;
   }
 
 }
