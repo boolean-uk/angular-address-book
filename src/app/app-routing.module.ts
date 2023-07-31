184fa96a231820f052fdd30aaaef26b31f8938e8
Szkaradd <szkaradekmikolaj@gmail.com>
2023-07-31 11:41:10 +0200

    created components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MenuComponent } from './layout/menu/menu.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { 
+    path: '',
+    component: MenuComponent,
+  },
+  {
+    
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -2,9 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { MenuComponent } from './menu/menu.component';
+import { ContactsListComponent } from './contacts-list/contacts-list.component';
+import { AddContactComponent } from './add-contact/add-contact.component';
+import { ViewContactComponent } from './view-contact/view-contact.component';
 
 @NgModule({
-  declarations: [MenuComponent],
+  declarations: [MenuComponent, ContactsListComponent, AddContactComponent, ViewContactComponent],
   imports: [CommonModule, RouterModule],
   exports: [MenuComponent],
 })
