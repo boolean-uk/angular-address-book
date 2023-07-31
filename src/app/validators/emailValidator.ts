56db4583eef233e4c2c8c5ff1e48cb9ddeaa741d
Rafal Halama <rafau.halama@gmail.com>
2023-07-31 12:53:09 +0200

    fixed regex

--- src/app/validators/emailValidator.ts
+++ src/app/validators/emailValidator.ts
@@ -4,7 +4,7 @@ import { Observable, of } from 'rxjs';
 export function asyncEmailValidator(
   control: AbstractControl
 ): Observable<ValidationErrors | null> {
-  const emailRegex: RegExp = /^[a-z0-9._%+-]+@[a-z0-9.-]+$/;
+  const emailRegex: RegExp = /^[a-z0-9._%+-]+@[a-z]{7}.[a-z]{2}.[a-z]{2}$/;
   const email: string = '' + control.value;
 
   if (!email.endsWith('@boolean.co.uk') || !emailRegex.test(email)) {
