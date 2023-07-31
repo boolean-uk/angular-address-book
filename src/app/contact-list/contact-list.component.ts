ec2b894d2467d6340c0d22f7650bf82457bd407c
himok <himok.pk@gmail.com>
2023-07-31 12:15:03 +0200

    contatsList working properly

--- src/app/contact-list/contact-list.component.ts
+++ src/app/contact-list/contact-list.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { ContactService, Contact } from "../contact.service";
 
 @Component({
   selector: 'app-contact-list',
-  templateUrl: '../../../templates/contact-list.html',
+  templateUrl: './contact-list.component.html',
   styleUrls: ['./contact-list.component.css']
 })
 export class ContactListComponent {
+  contacts: Contact[] = [];
+
+  constructor(private contactService: ContactService) {}
+
+  ngOnInit() {
+    this.contacts = this.contactService.getContacts()
+  }
 
 }
