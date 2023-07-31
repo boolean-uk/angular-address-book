87820681a11bb2d1aaa2447d853cc9338325e767
OliwiaKepczynska <oliwia.kepczynska@gmail.com>
2023-07-31 12:57:21 +0200

    adding html to contact list component

--- src/app/layout/contact-list/contact-list.component.ts
+++ src/app/layout/contact-list/contact-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-contact-list',
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./contact-list.component.css'],
 })
 export class ContactListComponent {
+  contacts: Contact[] = [];
 
 }
