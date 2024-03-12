41ac50af594cd570ac1177388ccd93bbed7d6240
Eddy1108 <edvardh@hotmail.no>
2024-03-12 14:38:22 +0100

    Core

---
+++ src/app/contacts/data/contacts.ts
@@ -0,0 +1,11 @@
+import { Contact } from '../models/contact';
+
+export const CONTACTS: Contact[] = [
+  {
+    id: 1,
+    firstName: "James",
+    lastName: "Machine",
+    street: "Baker Lane",
+    city: "Manchester"
+  },
+];
