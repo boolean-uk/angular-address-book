412c679c03c1a48f066ddf80041cce7552992f93
Alexander <alexander.ellnestam@gmail.com>
2024-03-12 08:21:33 +0100

    Done

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { MenuComponent } from './menu/menu.component';
+import { ContactListComponent } from './contact-list/contact-list.component';
+import { NewContactComponent } from './new-contact/new-contact.component';
+import { ViewContactComponent } from './view-contact/view-contact.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { ContactService } from './contact.service';
 
 @NgModule({
-  declarations: [MenuComponent],
-  imports: [CommonModule, RouterModule],
-  exports: [MenuComponent],
+  declarations: [MenuComponent, ContactListComponent, NewContactComponent, ViewContactComponent],
+  imports: [CommonModule, RouterModule, ReactiveFormsModule],
+  exports: [MenuComponent, ContactListComponent, NewContactComponent, ViewContactComponent],
 })
+
 export class LayoutModule {}
