b1ed5e6a226044edda2f445f4261a9429e49aa9e
Matthias Greeven <matsgreeven@gmail.com>
2024-03-12 12:28:25 +0100

    list inital

--- src/app/adressbook/list/list.component.ts
+++ src/app/adressbook/list/list.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { AdressbookService } from '../adressbook.service';
+import { Contact } from '../models/contact';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-list',
@@ -6,5 +9,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./list.component.css']
 })
 export class ListComponent {
-
+  constructor(private readonly adressBookService: AdressbookService) {}
+  contacts: Contact[] = this.adressBookService.contacts;
 }
