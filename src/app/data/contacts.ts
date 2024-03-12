4643bdb974a3c1b9dcf7f8fafe3da47f03946543
Knut S Rostad <knut.rostad@no.experis.com>
2024-03-12 15:51:03 +0100

    All done

---
+++ src/app/data/contacts.ts
@@ -0,0 +1,18 @@
+import { Contact } from '../contacts/models/contact';
+
+export const CONTACTS: Contact[] = [
+  {
+    id: 1,
+    firstName: 'Numba1',
+    lastName: 'Nameson',
+    street: 'Street 44',
+    city: 'Oslo',
+  },
+  {
+    id: 2,
+    firstName: 'Names',
+    lastName: 'Programmerson',
+    street: 'Angular road 32',
+    city: 'London',
+  },
+];
