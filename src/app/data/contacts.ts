352e77bfc4a9b9b404bdefaac63ce4fbf81f76f1
Espen Solhaug <espen.solhaug@no.experis.com>
2024-03-12 14:08:06 +0100

    core & extension finished

---
+++ src/app/data/contacts.ts
@@ -0,0 +1,18 @@
+import { Contact } from '../contacts/models/contact';
+
+export const CONTACTS: Contact[] = [
+  {
+    id: 1,
+    firstName: 'Joe',
+    lastName: 'Doe',
+    street: 'Baker street',
+    city: 'Southampton',
+  },
+  {
+    id: 1,
+    firstName: 'Peter',
+    lastName: 'Park',
+    street: 'Fifth Avenue',
+    city: 'New York',
+  },
+];
