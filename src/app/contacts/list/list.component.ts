77f26211df39e6af3907518d1d8ccc7f79feab70
Gudbrand <gudbrandwd@live.no>
2024-03-12 13:58:32 +0100

    core

---
+++ src/app/contacts/list/list.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { ContactsService } from '../contacts.service';
+import { Contact } from '../models/contact';
+
+@Component({
+  selector: 'app-list',
+  templateUrl: './list.component.html',
+  styleUrls: ['./list.component.css'],
+})
+export class ListComponent {
+  constructor(private readonly contactService: ContactsService) {}
+  contacts: Contact[] = this.contactService.contacts;
+}
