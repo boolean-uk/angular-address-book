e209e9c9d467229cb406307b758f2ce207d39b1c
ThomasKvam <thomas.e.h.kvam@hotmail.no>
2024-03-12 14:24:13 +0100

    Done with core

---
+++ src/app/people.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { People } from './data/people';
+import { Person } from './people/models/person';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PeopleService {
+  private currentId: number = People.length
+  public people: Person[] = People
+
+  public getAllPeople(): Person[]{
+    return this.people
+  }
+
+  public getPersonById(id: number | null): Person | null{
+    const person =this.people.find((person) => person.id === id)
+    if(!person){
+      return null
+    }
+    return person
+  }
+
+  public addPerson(person: Person){
+    this.people.push(person)
+  }
+
+  constructor() { }
+}
