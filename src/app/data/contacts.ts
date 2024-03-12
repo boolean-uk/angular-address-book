015fd8709aeecd01a535bdd7aae988898d5f0325
Gustav Svennas <gustavsvennas@gmail.com>
2024-03-12 16:05:14 +0100

    Done with core

---
+++ src/app/data/contacts.ts
@@ -0,0 +1,25 @@
+import { Contact } from "../contacts/models/contact";
+
+export const CONTACTS: Contact[] = [
+  {
+    id: 1,
+    firstName: 'Joe',
+    lastName: 'Biden',
+    street: '1600 Pennsylvania Avenue NW',
+    city: 'Washington, DC',
+  },
+  {
+    id: 2,
+    firstName: 'Charles',
+    lastName: 'III',
+    street: 'SW1A 1AA',
+    city: 'London',
+  },
+  {
+    id: 3,
+    firstName: 'Carl',
+    lastName: 'XVI Gustaf',
+    street: 'Kungliga slottet',
+    city: 'Stockholm',
+  },
+];
