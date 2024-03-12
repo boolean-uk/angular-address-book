30eb02fbd1c3fb94c817cf64782fea33614e082b
Aleksander Kolsrud <kols203@gmail.com>
2024-03-12 15:52:38 +0100

    Finished task

--- src/app/address-book-components/contact-view-page/contact-view-page.component.ts
+++ src/app/address-book-components/contact-view-page/contact-view-page.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ContactsService } from '../services/contacts.service';
+import { ActivatedRoute } from '@angular/router';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-contact-view-page',
@@ -6,5 +10,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./contact-view-page.component.css']
 })
 export class ContactViewPageComponent {
+    
+    private  route: ActivatedRoute = inject(ActivatedRoute)
+    private contactService: ContactsService = inject(ContactsService)
+    private router: Router = inject(Router)
+   
 
+  id: string | null = this.route.snapshot.paramMap.get('id');
+  singleContact = this.contactService.getSingleContact(Number(this.id))
+  
 }
+
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ContactsListComponentComponent } from './address-book-components/contacts-list-component/contacts-list-component.component';
 import { AddFormComponent } from './address-book-components/add-form/add-form.component';
+import { ContactViewPageComponent } from './address-book-components/contact-view-page/contact-view-page.component';
 
 const routes: Routes = [
   { path: 'contacts', component: ContactsListComponentComponent},
-  { path: 'contacts/add', component: AddFormComponent}
+  { path: 'contacts/add', component: AddFormComponent},
+  { path: 'contacts/contact/view/:id', component: ContactViewPageComponent}
 ];
 
 @NgModule({
