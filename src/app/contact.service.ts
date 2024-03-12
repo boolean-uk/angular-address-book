ba88773e68b4da26cf3c8c800cabbb2ba10068d7
stp <steventphung6@gmail.com>
2024-03-12 13:40:35 +0100

    Steven Tong Phung
    core complete

---
+++ src/app/contact.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+
+import { CONTACTS } from './data/contacts';
+import { Contact } from './contacts/models/contact';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ContactService {
+  public contacts: Contact[] = CONTACTS;
+
+  public getAllContacts(): Contact[] {
+    console.log(this.contacts);
+    return this.contacts;
+  }
+
+  public getContactById(id: number | null): Contact | null {
+    const contact = this.contacts.find((contact) => contact.id === id);
+    if (!contact) {
+      return null;
+    }
+    return contact;
+  }
+
+  public addNewContact(contact: Contact): void {
+    this.contacts.push(contact)
+  }
+
+
+
+
+
+  constructor() {}
+}
---
+++ src/app/data/contacts.ts
@@ -0,0 +1,11 @@
+import { Contact } from "../contacts/models/contact";
+
+export const CONTACTS: Contact[] = [
+    {
+        id:1,
+        firstName: 'Steven',
+        lastName: 'Phung',
+        street: "Bob's street",
+        city: 'Oslo'
+    },
+]
