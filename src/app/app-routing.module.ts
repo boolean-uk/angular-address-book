85d7bfadf91fe1abc205a1c14de2f4a986135511
Matthias Greeven <matsgreeven@gmail.com>
2024-03-12 12:28:05 +0100

    set routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListComponent } from './adressbook/list/list.component';
+import { ViewComponent } from './adressbook/view/view.component';
+import { CreateComponent } from './adressbook/create/create.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "contacts", component: ListComponent},
+  {path: "contacts/:id", component: ViewComponent},
+  {path: "contacts/create", component: CreateComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
