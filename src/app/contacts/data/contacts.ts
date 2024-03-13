24821776b93f5cd66283b2ff855450779875423a
Oneallane <oneallane2808@gmail.com>
2024-03-13 14:01:33 +0100

    core + extension complete

---
+++ src/app/contacts/data/contacts.ts
@@ -0,0 +1,15 @@
+import { Contact } from '../models/contact';
+
+export const CONTACTS: Contact[] = [{
+    id: 1,
+    firstName: 'Reidar',
+    lastName: 'Lane',
+    street: 'Hans Holmboes Gate',
+    city: 'Bergen'
+}, {
+    id: 2,
+    firstName: 'Per',
+    lastName: 'Gaymaen',
+    street: 'Drit i Fifa gaten',
+    city: 'K town yasbish'
+}]
