f13701615031716e354ea00c698ab9086cd96582
SBjer1 <sebastian.engan@no.experis.com>
2024-03-13 10:50:36 +0100

    Done core and extension

---
+++ src/app/contacts/list/list.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { ContactService } from '../contact.service';
+import {Contact} from '../models/contact';
+
+@Component({
+  selector: 'app-list',
+  templateUrl: './list.component.html',
+  styleUrls: ['./list.component.css']
+})
+export class ListComponent {
+  constructor(private readonly contactService: ContactService) {}
+  contacts: Contact[] = this.contactService.contacts;
+}
