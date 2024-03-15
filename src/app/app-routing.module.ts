2c73fad3f6f70219f4186b0c0490de544e8dff39
Andreas Lauvhjell <andreas.lauvhjell@no.experis.com>
2024-03-15 17:35:14 +0100

    Angular is.... not my cup of tea. Skellington made

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListComponent } from './addressbook/list/list.component';
+import { AddComponent } from './addressbook/add/add.component';
+import { ViewComponent } from './addressbook/view/view.component';
+import { EditComponent } from './addressbook/edit/edit.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: ListComponent },
+  { path: 'new', component: AddComponent },
+  { path: 'view/:id', component: ViewComponent },
+  { path: 'edit/:id', component: EditComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
