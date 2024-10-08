a259105b68a30018759b07fc0a11081865d4957b
gremble0 <hermanstornes@gmail.com>
2024-10-08 12:04:12 +0200

    Remove bad route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { MenuComponent } from './layout/menu/menu.component';
 import { ViewcontactsComponent } from './layout/viewcontacts/viewcontacts.component';
 import { CreatecontactComponent } from './layout/createcontact/createcontact.component';
 import { ViewcontactComponent } from './layout/viewcontact/viewcontact.component';
 import { EditcontactComponent } from './layout/editcontact/editcontact.component';
 
 const routes: Routes = [
-  { path: '', component: MenuComponent },
   { path: 'contacts', component: ViewcontactsComponent },
   { path: 'contacts/add', component: CreatecontactComponent },
   { path: 'contacts/:id', component: ViewcontactComponent },
