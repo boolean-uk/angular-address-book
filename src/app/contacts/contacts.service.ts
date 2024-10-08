43de094287b7f3eb4729e49e6e76bc8183d5cecf
Felix <felix.mathiasson99@gmail.com>
2024-10-08 16:00:51 +0200

    core done

---
+++ src/app/contacts/contacts.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import {Contact} from './models/contact'
+import {CONTACTS} from '../data/contacts'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ContactsService {
+  public contacts: Contact[] = CONTACTS
+
+  public AddContact(c: Contact) {
+    c.id = this.contacts.length + 1;
+    this.contacts.push(c);
+  }
+  public GetContactById(id: number | null) {
+    const contact = this.contacts.find((contact) => contact.id === id);
+    if(!contact) {
+      return null;
+    }
+    return contact;
+  }
+}
