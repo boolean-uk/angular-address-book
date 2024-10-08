44c7d8f0fff4a8f9888452afbc0eb77ac0e4ce29
Fredrik Haupt <fredrik.haupt@se.experis.com>
2024-10-08 14:05:55 +0200

    commit

---
+++ src/app/contacts/data/contacts.ts
@@ -0,0 +1,18 @@
+import { Contact } from "../contact/contact";
+
+export const CONTACTS: Contact[] = [
+    {
+        id: 1,
+        firstName: "Robert",
+        lastName: "Tables",
+        street: "Form Avenue",
+        city: "Mc'City"
+    },
+    {
+        id: 2,
+        firstName: "Steve",
+        lastName: "Chair",
+        street: "Road st.",
+        city: "Mc'City"
+    },
+]
