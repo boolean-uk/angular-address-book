ccede3dc44e1c71981cdfa0b7aa9eeb1510e0c92
Joaquin Lindkvist <joaquinzache@gmail.com>
2024-10-08 16:30:28 +0200

    Core and Extensions Completed!

---
+++ src/app/contacts/edit/edit.component.ts
@@ -0,0 +1,43 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ContactsService } from '../contacts.service';
+import { Contact } from '../models/contact';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-edit',
+  templateUrl: './edit.component.html',
+  styleUrl: './edit.component.css'
+})
+export class EditComponent {
+  contactForm:FormGroup;
+  cService:ContactsService;
+  contact:Contact|null = null;
+  constructor(private formBuilder:FormBuilder, private readonly contactsService:ContactsService, private readonly route:ActivatedRoute) {
+    this.contactForm = this.formBuilder.group({
+      firstName:['', Validators.required],
+      lastName:['', Validators.required],
+      street:['', Validators.required],
+      city:['', Validators.required]
+    });
+    this.cService = this.contactsService;
+    this.contact = this.contactsService.GetContactById(Number(route.snapshot.paramMap.get('id')));
+
+    if(this.contact) {
+      this.contactForm.patchValue(this.contact);
+    }
+  }
+
+  editContact(): void {
+    if(this.contact) {
+      const updatedContact: Contact = {
+        id: this.contact.id,
+        firstName: this.contactForm.value.firstName,
+        lastName: this.contactForm.value.lastName,
+        street: this.contactForm.value.street,
+        city: this.contactForm.value.city
+      };
+      this.cService.EditContact(updatedContact);
+    }
+  }
+}
---
+++ src/app/data/contacts.ts
@@ -0,0 +1,18 @@
+import { Contact } from "../contacts/models/contact";
+
+export const CONTACTS:Contact[] = [
+    {
+        id: 1,
+        firstName: 'John',
+        lastName: 'Carmack',
+        street: '10 Keen Street',
+        city: 'London'
+    },
+    {
+        id: 2,
+        firstName: 'Grace',
+        lastName: 'Hopper',
+        street: '32 Deebug Road',
+        city: 'London'
+    }
+]
