11d330cfe0bbb4d8d6ea483e623017521422d287
Thomas Wiik <thomas.wiik@no.experis.com>
2024-10-08 14:40:07 +0200

    Set up foundation.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { AddComponent } from './components/add/add.component';
+import { EditComponent } from './components/edit/edit.component';
+import { ListComponent } from './components/list/list.component';
+import { ViewComponent } from './components/view/view.component';
+
+const routes: Routes = [
+  {path: '', component: ContactListComponent },
+  {path: 'add', component: AddContactComponent},
+  {path: 'contact/:id', component: ViewContactComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
---
+++ src/app/data/contacts.ts
@@ -0,0 +1,18 @@
+import { Contact } from "../components/models/contact.model";
+
+export const CONTACTS: Contact[] = [
+    {
+        id: 1,
+        firstName: "A",
+        lastName: "B",
+        street: "C street",
+        city: "D city"
+    },
+    {
+        id: 2,
+        firstName: "E",
+        lastName: "Frank",
+        street: "G street",
+        city: "H city"
+    }
+]
