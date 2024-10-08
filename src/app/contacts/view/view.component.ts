1cd0dd2abc877734cecc36f955a5b2b95a529105
Ludwig J Lundborg <ludwig.j.lundborg@gmail.com>
2024-10-08 15:02:05 +0200

    Added correct view for  route path

--- src/app/contacts/view/view.component.ts
+++ src/app/contacts/view/view.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ContactService } from 'src/app/contact.service';
+import { Contact } from '../models/contact';
 
 @Component({
   selector: 'app-view',
@@ -6,5 +9,10 @@ import { Component } from '@angular/core';
   styleUrl: './view.component.css'
 })
 export class ViewComponent {
+  contactService = inject(ContactService);
+  route = inject(ActivatedRoute);
 
+  id = this.route.snapshot.paramMap.get('id')
+  contact: Contact | null = this.contactService.getContactById(Number(this.id));
+  
 }
