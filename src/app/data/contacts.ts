8e6fdb6b94ba23e373421b9585891961c4de29b9
Silje Gjesdal Jacobsen <siljegja@gmail.com>
2024-10-08 13:56:28 +0200

    finished core and extensiosn

---
+++ src/app/data/contacts.ts
@@ -0,0 +1,40 @@
+import { Contact } from "../contacts/models/contact"
+
+// a collection of contacts
+export const CONTACTS: Contact[] = [
+    {
+        id: 1,
+        firstName: 'Eyvind',
+        lastName: 'Mjalde',
+        street: 'Landås',
+        city: 'Bergen'
+    },
+    {
+        id: 2,
+        firstName: 'Espen',
+        lastName: 'Luna',
+        street: 'Knarvik',
+        city: 'Bergen'
+    },
+    {
+        id: 3,
+        firstName: 'Daniel',
+        lastName: 'Rølli',
+        street: 'Wergeland',
+        city: 'Bergen'
+    },
+    {
+        id: 4,
+        firstName: 'Øyvind',
+        lastName: 'Onarheim',
+        street: 'Nordås',
+        city: 'Bergen'
+    },
+    {
+        id: 5,
+        firstName: 'Silje',
+        lastName: 'Jacobsen',
+        street: 'Landås',
+        city: 'Bergen'
+    }
+]
