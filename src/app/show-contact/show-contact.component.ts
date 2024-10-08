b79af58609dc18fb505b8d6a639d87ad38cded2f
Stian Rusvik <stian.rusvik@no.experis.com>
2024-10-08 11:41:09 +0200

    added neccessary components.

---
+++ src/app/show-contact/show-contact.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-show-contact',
+  standalone: true,
+  imports: [],
+  templateUrl: './show-contact.component.html',
+  styleUrl: './show-contact.component.css'
+})
+export class ShowContactComponent {
+
+}
