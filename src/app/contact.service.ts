d1ceaef961c716488a99f9e51296ab6bf1570219
Max V Olofsson <max.olofsson@se.experis.com>
2024-10-08 13:13:58 +0200

    Done with done.

---
+++ src/app/contact.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { Contact } from './contacts/models/contact';
+import { CONTACTS } from './contacts/data/contacts';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ContactsService {
+  private currentId: number = 2; // Starting at 2 since we have two example data contacts already
+  private contacts: Contact[] = CONTACTS;
+
+  public getContactById(id: number | null): Contact | null {
+    const contact = this.contacts.find((contacts) => contacts.id === id);
+    if (!contact) {
+      return null;
+    }
+    return contact;
+  }
+
+  public getAllContacts(): Contact[] {
+    return this.contacts;
+  }
+
+  public addContact(contact: Contact) {
+    this.currentId++;
+    this.contacts.push({ ...contact, id: this.currentId });
+  }
+}
---
+++ src/app/contacts/data/contacts.ts
@@ -0,0 +1,18 @@
+import { Contact } from '../models/contact';
+
+export const CONTACTS: Contact[] = [
+  {
+    id: 1,
+    firstName: 'Max',
+    lastName: 'Olofsson',
+    street: 'Street name 10',
+    city: 'Gothenburg',
+  },
+  {
+    id: 2,
+    firstName: 'Sven',
+    lastName: 'Svensson',
+    street: 'Street name 5',
+    city: 'Stockholm',
+  },
+];
