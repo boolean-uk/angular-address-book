972403bfcf9642bad840c8cc720771c733225d4f
Simon Fredriksson <128689713+SimpFred@users.noreply.github.com>
2024-10-08 14:17:28 +0200

    Refactor CSS styles and created components for the page
    - Refactored CSS styles in app.component.css and menu.component.css for improved layout and hover effects.
    - Created components to display, add and edit contacts.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,31 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListComponent } from './contact/list/list.component';
+import { AddComponent } from './contact/add/add.component';
+import { ViewComponent } from './contact/view/view.component';
+import { EditComponent } from './contact/edit/edit.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: ListComponent,
+  },
+  {
+    path: 'contacts/add',
+    component: AddComponent,
+  },
+  {
+    path: 'contacts/:id',
+    component: ViewComponent,
+  },
+  {
+    path: 'contacts/:id/edit',
+    component: EditComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
