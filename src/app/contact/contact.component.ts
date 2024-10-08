22e5a8edfba9c35dbd6ee5a6726183750638cd9d
Iris Flystam <badde00@gmail.com>
2024-10-08 15:58:25 +0200

    basic reqs

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,14 +1,28 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Contact } from './contact';
+import { ActivatedRoute } from '@angular/router';
+import { ContactService } from './contact.service';
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css'
 })
-export class ContactComponent {
-  @Input() id: number = 0;
-  @Input() firstName: string = '';
-  @Input() lastName: string = '';
-  @Input() street: string = '';
-  @Input() city: string = '';
+export class ContactComponent implements OnInit {
+  contact: Contact | null = null;
+
+  constructor(
+    private route: ActivatedRoute,
+    private contactService: ContactService,
+  ) {}
+
+  ngOnInit() {
+      const id = Number(this.route.snapshot.paramMap.get('id'));
+      this.contact = this.contactService.getContactById(id);
+
+      if (!this.contact) {
+        this.contact = null;
+        console.error('Contact not found');
+      }
+  }
 }
--- src/app/contact/contact.module.ts
+++ src/app/contact/contact.module.ts
@@ -3,12 +3,12 @@ import { AddContactComponent } from "../add-contact/add-contact.component";
 import { ContactListComponent } from "../contact-list/contact-list.component";
 import { ContactComponent } from "./contact.component";
 import { RouterModule } from "@angular/router";
-import { ReactiveFormsModule } from "@angular/forms";
-import { NgFor } from "@angular/common";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { NgFor, NgIf } from "@angular/common";
 
 @NgModule({
     declarations: [AddContactComponent, ContactListComponent, ContactComponent],
-    imports: [ReactiveFormsModule, RouterModule, NgFor],
+    imports: [ReactiveFormsModule, RouterModule, NgFor, FormsModule, NgIf],
     exports: [AddContactComponent, ContactListComponent, ContactComponent]
 })
 export class ContactModule {}
