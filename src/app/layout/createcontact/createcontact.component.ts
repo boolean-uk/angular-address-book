6af7db699211a614e1a0f45c53fc630b255e9d7b
gremble0 <hermanstornes@gmail.com>
2024-10-08 09:16:07 +0200

    Basic routing

---
+++ src/app/layout/createcontact/createcontact.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-createcontact',
+  standalone: true,
+  imports: [],
+  templateUrl: './createcontact.component.html',
+})
+export class CreatecontactComponent {}
--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 import { MenuComponent } from './menu/menu.component';
+import { CreatecontactComponent } from './createcontact/createcontact.component';
+
+const routes: Routes = [
+  { path: '', component: MenuComponent },
+  { path: 'add', component: CreatecontactComponent },
+];
 
 @NgModule({
   declarations: [MenuComponent],
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule.forRoot(routes)],
   exports: [MenuComponent],
 })
 export class LayoutModule {}
