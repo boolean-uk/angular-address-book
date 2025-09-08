f0039c8c49f66146a42c70897a570080410692f5
Hans Jakob Håland <Hans.Jakob.Haland@no.experis.com>
2025-09-08 11:46:36 +0200

    Adds contacts list view

---
+++ src/app/contacts/list/list.component.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { Component } from '@angular/core';
+import { Contact } from 'src/app/models/contact';
+import { ContactsService } from 'src/app/services/contacts.service';
+
+@Component({
+  selector: 'app-list',
+  standalone: false,
+  templateUrl: './list.component.html',
+  styleUrl: './list.component.css'
+})
+export class ListComponent {
+  contacts: Contact[] = [];
+  constructor(private readonly contactsService: ContactsService) {
+    this.contacts = this.contactsService.contacts;
+  }
+}
