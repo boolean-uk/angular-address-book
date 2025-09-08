da75f3e367b4d815cb05a4eb04f929b02b06a3ee
Marie Helene Hansen <marhelhan@icloud.com>
2025-09-08 11:49:41 +0200

    core

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { CommonModule } from '@angular/common';
 import { LayoutModule } from './layout/layout.module';
+import { ContactsModule } from './contacts/contacts.module';
+import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, LayoutModule],
-  bootstrap: [AppComponent],
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    CommonModule,
+    LayoutModule,
+    ContactsModule
+  ],
+  providers: [
+  ],
+  bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
---
+++ src/app/contacts/models/contact.ts
@@ -0,0 +1,10 @@
+export interface Contact {
+    // any object of type Car needs to have this shape
+    // kind of like blueprint like Java??? 
+
+    id: number;
+    firstName: string;
+    lastName: string;
+    street: string;
+    city: string;
+}
